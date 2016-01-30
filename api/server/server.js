71931d3ede2ae88eb5ee386f61f4b7b5e3518f94
elanini <elanini@ucsc.edu>
2016-01-30 10:28:02 -0800

    add pid file for easy killing

--- api/server/server.js
+++ api/server/server.js
@@ -1,5 +1,6 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var npid = require('npid');
 
 var app = module.exports = loopback();
 app.start = function() {
@@ -11,6 +12,14 @@ app.start = function() {
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+
+    // save PID file
+    try {
+      var pid = npid.create('/root/server.pid');
+      pid.removeOnExit();
+    } catch (err) {
+      console.log('couldnt create PID')
     }
   });
 };
