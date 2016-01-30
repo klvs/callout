5ff214b0eb8f25d05bb66b5293c45ba833e92a7d
Jerry Chen <admin@close.cc>
2016-01-30 14:24:28 -0800

    adding support for voting

--- api/common/models/callout.js
+++ api/common/models/callout.js
@@ -3,13 +3,8 @@ const fileType = require('file-type')
 
 module.exports = function(Callout) {
 	Callout.observe('before save', function uploadToS3(ctx, next) {
-		var data;
-		if(ctx.instance)
-			data = ctx.instance;
-		else
-			data = ctx.data;
-
-		data.time = new Date();
+		if (ctx.instance) // only change the time if this is a new callout
+			ctx.instance.time = new Date();
 
 		next();
 	});
