0a6645fc913a11ccfb80bebe6a1a740ce1efab8d
Kyle Salter <ksalter@ucsc.edu>
2016-01-30 15:42:24 -0800

    Updated readme for instructions to query callouts near to a location. Added upvote and downvote endpoints for callout

--- api/common/models/callout.js
+++ api/common/models/callout.js
@@ -8,4 +8,41 @@ module.exports = function(Callout) {
 
 		next();
 	});
+	Callout.upvote = function(calloutId, cb) {
+		applyVote(calloutId, 1, cb)
+	};
+	Callout.downvote = function(calloutId, cb) {
+		applyVote(calloutId, -1, cb)
+	};
+	function applyVote(calloutId, value, cb) {
+		Callout.findById(calloutId, function(err, callout) {
+			if(err)
+				cb(err, 0);
+			else {
+				callout.voteCount += value;
+				callout.save(function(err, callout) {
+					if(err)
+						cb(err, value);
+					else
+						cb(null, value);
+				});
+			}
+		});
+	};
+	Callout.remoteMethod(
+		'upvote',
+		{
+			http: { path: '/:id/upvote', verb: 'post' },
+			accepts: { ard: 'id', type: 'string', required: true },
+			returns: { arg: 'voteCount', type: 'number' }	
+		}
+	);
+	Callout.remoteMethod(
+		'downvote',
+		{
+			http: { path: '/:id/downvote', verb: 'post' },
+			accepts: { ard: 'id', type: 'string', required: true },
+			returns: [ { arg: 'err', type: 'string' }, { arg: 'voteCount', type: 'number' } ]	
+		}
+	);
 };
