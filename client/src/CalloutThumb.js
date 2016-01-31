d778513eaa52ee5e289216a6ca334bef30b28bbc
Jerry Chen <admin@close.cc>
2016-01-31 00:23:22 -0800

    h3

--- client/src/CalloutThumb.js
+++ client/src/CalloutThumb.js
@@ -34,7 +34,7 @@ export default class CalloutThumb extends Component {
   render() {
     return (
       <Col style={colStyle} xs={4} sm={4} md={4} lg={4}>
-      	<h2 style={styles}>{this.props.data.desc.title}</h2>
+      	<h3 style={styles}>{this.props.data.desc.title}</h3>
       	<Image style={imgStyle} className="img-responsive" src={this.props.data.url} />
       </Col>
     );
