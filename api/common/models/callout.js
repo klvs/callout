6e4e039c3329d8ef55868d7706c8b67b57f6379e
Kyle Salter <ksalter@ucsc.edu>
2016-01-30 22:46:20 -0800

    changed vote api to not rely on url id

--- api/common/models/callout.js
+++ api/common/models/callout.js
@@ -1,6 +1,8 @@
 var shortid = require('shortid')
 const fileType = require('file-type')
 
+var calloutVoteThreshold = 50;
+
 module.exports = function(Callout) {
 	Callout.observe('before save', function uploadToS3(ctx, next) {
 		if (ctx.instance) // only change the time if this is a new callout
@@ -20,6 +22,7 @@ module.exports = function(Callout) {
 				cb(err, 0);
 			else {
 				callout.voteCount += value;
+				if 
 				callout.save(function(err, callout) {
 					if(err)
 						cb(err, value);
@@ -32,16 +35,16 @@ module.exports = function(Callout) {
 	Callout.remoteMethod(
 		'upvote',
 		{
-			http: { path: '/:id/upvote', verb: 'post' },
-			accepts: { ard: 'id', type: 'string', required: true },
+			http: { path: '/upvote', verb: 'post' },
+			accepts: { arg: 'id', type: 'string', required: true },
 			returns: { arg: 'voteCount', type: 'number' }	
 		}
 	);
 	Callout.remoteMethod(
 		'downvote',
 		{
-			http: { path: '/:id/downvote', verb: 'post' },
-			accepts: { ard: 'id', type: 'string', required: true },
+			http: { path: '/downvote', verb: 'post' },
+			accepts: { arg: 'id', type: 'string', required: true },
 			returns: [ { arg: 'err', type: 'string' }, { arg: 'voteCount', type: 'number' } ]	
 		}
 	);
